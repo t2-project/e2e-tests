eade4bd783bfde3e298ca26a23d8dec85d8c097e
Sarah Sophie Stieß <st115875@stud.uni-stuttgart.de>
2021-05-05 15:25:03 +0200

    init project

---
+++ src/main/java/de/unistuttgart/t2/e2etest/E2etestApplication.java
@@ -0,0 +1,13 @@
+package de.unistuttgart.t2.e2etest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E2etestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E2etestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/unistuttgart/t2/e2etest/E2etestApplicationTests.java
@@ -0,0 +1,13 @@
+package de.unistuttgart.t2.e2etest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E2etestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
